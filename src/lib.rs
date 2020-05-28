4a5d001ff3cfe26b2831009a3e035010b5569278
kojakatsuma <katsuma.koja@ubiregi.com>
2020-05-29 07:41:58 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use wasm_bindgen::prelude::*;
+extern crate web_sys;
+
+
+// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
+// allocator.
+//
+// If you don't want to use `wee_alloc`, you can safely delete this.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+
+// This is like the `main` function, except for JavaScript.
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(debug_assertions)]
+    console_error_panic_hook::set_once();
+    let window = web_sys::window().expect("no global `window` exists");
+    window.alert_with_message("Hello World !!!").unwrap();
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust!");
+    body.append_child(&val)?;
+    // Your code goes here!
+    web_sys::console::log_1(&JsValue::from_str("Hello world!"));
+
+    Ok(())
+}
